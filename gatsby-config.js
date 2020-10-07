4b2243b4bbf98f35c7f572b856894baf29eef357
Exanderal <maksymilian.andrzejewski@gmail.com>
2020-10-07 23:39:03 +0200

    Setup for stack overflow

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,13 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    {
+      resolve: "gatsby-plugin-load-script",
+      options: {
+        src:
+          "https://mapa.ecommerce.poczta-polska.pl/widget/scripts/ppwidget.js",
+      },
+    },
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
